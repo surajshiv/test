1773b60bbc067ccafa67cfbe89a00631ea4409de
Ashutosh Kandpal <ashutosh.kandpal@ericsson.com>
2019-03-12 22:31:34 -0500

    now 1

--- sample_1.py
+++ sample_1.py
@@ -3,10 +3,3 @@ if pythonic=='yes':
     print('do coding')
 else:
     print('learn code')
-<<<<<<< HEAD
-        print('will commit')
-    
-=======
-	print('learn marcos')
-    
->>>>>>> parent of 908a8ef... added marcos removed
